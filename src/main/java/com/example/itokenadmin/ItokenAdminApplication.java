c3ac7152f9c1d2961f2c6714d96fb36e8e45c2d4
yls <1329926763@qq.com>
2019-09-16 15:06:03 +0800

    1

---
+++ src/main/java/com/example/itokenadmin/ItokenAdminApplication.java
@@ -0,0 +1,17 @@
+package com.example.itokenadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@EnableAdminServer
+@SpringBootApplication
+public class ItokenAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItokenAdminApplication.class, args);
+    }
+
+}
